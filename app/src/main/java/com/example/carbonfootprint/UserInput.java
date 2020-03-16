757ccc7280904a97ec8afe322f6ebcf3ceeb7284
alistair <fjb19161@uni.strath.ac.uk>
2020-03-16 16:26:31 +0000

    Rigged input pages 1 and 2 to record dummy values and proceed to results page (skips page 3 for now). Will refactor later because it's dirty.

--- app/src/main/java/com/example/carbonfootprint/UserInput.java
+++ app/src/main/java/com/example/carbonfootprint/UserInput.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class UserInput extends AppCompatActivity {
 
@@ -12,11 +14,43 @@ public class UserInput extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_input);
+        findViewById(R.id.answer).setTag(0f);
+        findViewById(R.id.answer2).setTag(10f);
+        findViewById(R.id.answer3).setTag(20f);
+        findViewById(R.id.answer4).setTag(30f);
+        findViewById(R.id.answer5).setTag(40f);
+
+        findViewById(R.id.answer6).setTag(0f);
+        findViewById(R.id.answer7).setTag(10f);
+        findViewById(R.id.answer8).setTag(20f);
+        findViewById(R.id.answer9).setTag(30f);
+        findViewById(R.id.answer10).setTag(40f);
+
+        findViewById(R.id.answer11).setTag(0f);
+        findViewById(R.id.answer12).setTag(10f);
+        findViewById(R.id.answer13).setTag(20f);
+        findViewById(R.id.answer14).setTag(30f);
+        findViewById(R.id.answer15).setTag(40f);
     }
 
     public void electric(View view)
     {
-        Intent next = new Intent(this, userInput2.class);
+       RadioGroup question1 = findViewById(R.id.radioGroup);
+        RadioButton answer1 = findViewById(question1.getCheckedRadioButtonId());
+        Float a = (Float) answer1.getTag();
+        EmissionCalculatorTable.calculateAndRecord("type_plane", a);
+
+        RadioGroup question2 = findViewById(R.id.radioGroup2);
+        RadioButton answer2 = findViewById(question2.getCheckedRadioButtonId());
+        Float b = (Float) answer2.getTag();
+        EmissionCalculatorTable.calculateAndRecord("type_car", b);
+
+        RadioGroup question3 = findViewById(R.id.radioGroup2);
+        RadioButton answer3 = findViewById(question3.getCheckedRadioButtonId());
+        Float c = (Float) answer3.getTag();
+        EmissionCalculatorTable.calculateAndRecord("type_train", c);
+
+        Intent next = new Intent(this, UserInput2.class);
         startActivity(next);
     }
 }
